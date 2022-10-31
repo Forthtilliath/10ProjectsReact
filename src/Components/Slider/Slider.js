fc68d501e0fa255b2c7f11b18678b4fd120da39f
Forth <forth@live.fr>
2022-10-31 01:59:11 +0000

    Update Slider.js

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,112 +1,97 @@
-import React, {useState} from 'react'
-import {useEffect} from 'react'
-import './Slider.css';
-import dataSlider from './dataSlider';
-import BtnSlider from './BtnSlider';
-
+import React, { useState } from "react";
+import { useEffect } from "react";
+import "./Slider.css";
+import dataSlider from "./dataSlider";
+import BtnSlider from "./BtnSlider";
 
 export default function Slider() {
+  const [slideAnim, setSlideAnim] = useState({
+    index: 1,
+    inProgress: false,
+  });
 
-    const [slideAnim, setSlideAnim] = useState({
-        index: 1,
-        inProgress: false
-    })
+  const autoSlide = () => {
+    const si = setInterval(nextSlide, 3000);
+    return () => clearInterval(si);
+  };
 
-    const autoSlide = () => {
+  const nextSlide = () => {
+    if (slideAnim.inProgress) return;
 
-        if(slideAnim.index !== dataSlider.length && !slideAnim.inProgress){
-            setSlideAnim({index: slideAnim.index, inProgress: false})
+    setSlideAnim((anim) => ({
+      index: anim.index === dataSlider.length ? 1 : anim.index + 1,
+      inProgress: true,
+    }));
 
-            setTimeout(() => {
-                setSlideAnim({index: slideAnim.index + 1, inProgress: false})
-            }, 400)
+    setTimeout(() => {
+      setSlideAnim((anim) => ({
+        ...anim,
+        inProgress: false,
+      }));
+    }, 400);
+  };
 
-        }
-        else if(slideAnim.index === dataSlider.length && !slideAnim.inProgress){
-            setSlideAnim({index: 1, inProgress: true});
-            setTimeout(() => {
-                setSlideAnim({index: 1, inProgress: false})
-            }, 400)
-        }
-    }
+  const prevSlide = () => {
+    if (slideAnim.inProgress) return;
 
+    setSlideAnim((anim) => ({
+      index: anim.index === 1 ? dataSlider.length : anim.index - 1,
+      inProgress: true,
+    }));
 
-    const nextSlide = () => {
+    setTimeout(() => {
+      setSlideAnim((anim) => ({
+        ...anim,
+        inProgress: false,
+      }));
+    }, 400);
+  };
 
-        if(slideAnim.index !== dataSlider.length && !slideAnim.inProgress){
-            setSlideAnim({index: slideAnim.index + 1, inProgress: true})
+  const moveDot = (index) => {
+    setSlideAnim({ index, inProgress: false });
+  };
 
-            setTimeout(() => {
-                setSlideAnim({index: slideAnim.index + 1, inProgress: false})
-            }, 400)
+  useEffect(() => {
+    // const si = setInterval(nextSlide, 3000);
+    // return () => clearInterval(si);
+    return autoSlide();
+  }, []);
 
-        }
-        else if(slideAnim.index === dataSlider.length && !slideAnim.inProgress){
-            setSlideAnim({index: 1, inProgress: true});
-            setTimeout(() => {
-                setSlideAnim({index: 1, inProgress: false})
-            }, 400)
-        }
-
-    }
-
-    const prevSlide = () => {
-        
-        if(slideAnim.index !== 1 && !slideAnim.inProgress){
-            setSlideAnim({index: slideAnim.index - 1, inProgress: true});
-            setTimeout(() => {
-                setSlideAnim({index: slideAnim.index - 1, inProgress: false})
-            }, 400)
-        }
-        else if(slideAnim.index === 1 && !slideAnim.inProgress){
-            setSlideAnim({index: dataSlider.length, inProgress: true});
-            setTimeout(() => {
-                setSlideAnim({index: dataSlider.length, inProgress: false})
-            }, 400)
-        }
-
-    }
-
-    const moveDot = index => {
-        setSlideAnim({index: index, inProgress: false})
-    }
-
-    useEffect(()=>{
-
-        setTimeout(() => {
-            nextSlide()
-        }, 3000);
-
-    }, [])
-
+  useEffect(() => {
+    console.log(slideAnim);
+  }, [slideAnim]);
 
   return (
     <div className="container-slider">
-        {dataSlider.map((obj, index) => {
-            return(
-                <div
-                key={obj.id}
-                className={slideAnim.index === index + 1 ? "slide active-anim" : "slide"}
-                >
-                    <img src={process.env.PUBLIC_URL + `/Imgs/img${index+1}.jpg`} alt="Image slider" />
-                </div>
-            )
+      {dataSlider.map((obj, index) => {
+        return (
+          <div
+            key={obj.id}
+            className={
+              slideAnim.index === index + 1 ? "slide active-anim" : "slide"
+            }
+          >
+            <img
+              src={process.env.PUBLIC_URL + `/Imgs/img${index + 1}.jpg`}
+              alt="Image slider"
+            />
+          </div>
+        );
+      })}
+
+      <BtnSlider moveSlide={prevSlide} direction={"prev"} />
+      <BtnSlider moveSlide={nextSlide} direction={"next"} />
+
+      <div className="container-dots">
+        {Array.from({ length: 5 }).map((_, index) => {
+          return (
+            <div
+              className={slideAnim.index === index + 1 ? "dot active" : "dot"}
+              onClick={() => moveDot(index + 1)}
+            ></div>
+          );
         })}
-
-        <BtnSlider moveSlide={prevSlide} direction={"prev"}/>
-        <BtnSlider moveSlide={nextSlide} direction={"next"}/>
-
-        <div className="container-dots">
-
-            {Array.from({length: 5}).map((item, index) => {
-                return <div 
-                className={slideAnim.index === index+1 ? "dot active" : "dot"}
-                onClick={() => moveDot(index + 1)}
-                >
-                </div>
-            })}
-
-        </div>
+      </div>
     </div>
-  )
+  );
 }
